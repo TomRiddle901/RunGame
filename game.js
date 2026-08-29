c17d1c593926303e871c9c83262df81428c08a9c
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-08-29 15:14:14 +0200

    La casella di input e il bottone non devono essere inizializzati a `NULL`

--- game.js
+++ game.js
@@ -5,8 +5,8 @@ let canvasHeight = 500;
 // Impostazioni stato del gioco e variabili globali
 let gameState = 'START'; // Stato del gioco: 'START', 'RACING', FINISH'
 let winner = null;
-let inputRisposta = null; // Testo in input risposta domande
-let btnInvia = null; // Bottone per inviare la risposta
+let inputRisposta; // Testo in input risposta domande
+let btnInvia; // Bottone per inviare la risposta
 
 // Giocatori
 let startLineX = 70; // Posizione di partenza
